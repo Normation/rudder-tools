d872f1924e851fdc5635eb06fd364af07caf68be
Jonathan Clarke <jonathan.clarke@normation.com>
2014-02-18 16:04:36 +0100

    Add tool to create a Rudder server in EC2 and import 1000 fake nodes

---
+++ scripts/ec2-rudder-server-and-fake-nodes/create-instance.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#####################################################################################
+# Copyright 2014 Normation SAS
+#####################################################################################
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, Version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#####################################################################################
+
+set -e
+
+# Avoid any environment pollution that may cause package installation to fail
+export LANG=C LC_ALL=C
+
+# Configuration start
+REGION_ID="eu-west-1"
+SECURITY_GROUP="sg-b48bd9c3" # Change this to a security group you have setup in EC2
+UBUNTU_1210_64=ami-34af5f43
+IMAGE_ID=${UBUNTU_1210_64}
+INSTANCE_TYPE="m3.medium" # Currently 3.75 GiB of RAM + 1 x 4 GB SSD, try m3.large for more RAM/disk space
+# Configuration end
+
+# Get user data content
+USER_DATA=`./generate-mime-message ssh-keys:cloud-config install-script.sh:x-shellscript`
+
+# Make sure we have euca2ools
+if ! euca-run-instances --version > /dev/null; then
+  echo "Can't run euca-run-instances. Please install euca2ools (apt-get install euca2tools)"
+  exit 1
+fi
+
+# Start a server
+INSTANCE_ID=`euca-run-instances --region ${REGION_ID} -t ${INSTANCE_TYPE} -g ${SECURITY_GROUP} --user-data "${USER_DATA}" ${IMAGE_ID} | grep INSTANCE | sed -r "s/^INSTANCE[\t ]*([a-z0-9-]+)[\t ]+.*$/\1/"`
+
+# Print out instance info
+echo "Instance started, ID is ${INSTANCE_ID}."
+echo -n "Waiting for hostname to be available..."
+FOUND=0
+while [ ${FOUND} -ne 1 ]; do
+  HOSTNAME=`euca-describe-instances --region ${REGION_ID} ${INSTANCE_ID} | grep "^INSTANCE" | cut -f4`
+  if [ "z${HOSTNAME}" != "z" ]; then
+    FOUND=1
+    echo " OK!"
+  else
+    echo -n "."
+    sleep 1
+  fi
+done
+
+echo "You can now connect to it using:"
+echo "ssh ubuntu@${HOSTNAME}"
+echo "https://${HOSTNAME}"
---
+++ scripts/ec2-rudder-server-and-fake-nodes/install-script.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+#####################################################################################
+# Copyright 2014 Normation SAS
+#####################################################################################
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, Version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#####################################################################################
+
+set -e
+
+# Avoid any environment pollution that may cause package installation to fail
+export LANG=C LC_ALL=C
+
+# VARS
+SERVER_INSTANCE_HOST=`curl http://169.254.169.254/latest/meta-data/public-hostname`
+DEMOSAMPLE="no"
+LDAPRESET="yes"
+INITPRORESET="yes"
+ALLOWEDNETWORK[0]='127.0.0.1/8'
+
+# Import GPG key
+apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 474A19E8 >> /tmp/log
+
+# Add Rudder repository and update local package database
+echo "deb http://www.rudder-project.org/apt-2.10-nightly/ $(lsb_release -cs) main" > /etc/apt/sources.list.d/rudder.list
+aptitude update >> /tmp/log
+
+# Install Rudder server components
+aptitude install -y rudder-server-root >> /tmp/log
+
+# Allow all connections to LDAP and PostgreSQL
+sed -i "s/^IP=.*$/IP=*/" /etc/default/slapd >> /tmp/log
+/etc/init.d/slapd restart >> /tmp/log
+echo "listen_addresses = '*'" >> /etc/postgresql/*/main/postgresql.conf
+echo "host    all         all         192.168.90.0/24       trust" >> /etc/postgresql/*/main/pg_hba.conf
+echo "host    all         all         10.94.94.0/24       trust" >> /etc/postgresql/*/main/pg_hba.conf
+/etc/init.d/postgresql restart >> /tmp/log
+
+# Initialize Rudder
+/opt/rudder/bin/rudder-init.sh $SERVER_INSTANCE_HOST $DEMOSAMPLE $LDAPRESET $INITPRORESET ${ALLOWEDNETWORK[0]} >> /tmp/log
+
+# Install some basic stats tools and Tsung and Perl-lib for UUID generation
+aptitude install -y htop iotop tsung libdata-uuid-perl
+
+# Get a sample inventory file and scripts to generate random hostnames/MACs
+cd /tmp
+#curl https://raw2.github.com/Normation/ldap-inventory/master/inventory-fusion/src/test/resources/fusion-report/centos-6-64-2011-09-22-12-02-27.ocs
+wget https://github.com/Normation/rudder-tools/raw/master/contrib/stress_suite/genhosts.pl
+wget https://github.com/Normation/rudder-tools/raw/master/contrib/stress_suite/inv.ocs
+wget https://github.com/Normation/rudder-tools/raw/master/contrib/stress_suite/tsung.xml
+
+# Generate hosts data
+perl /tmp/genhosts.pl > /tmp/hosts
+
+# Run tsung to load nodes into Rudder
+tsung -f /tmp/tsung.xml start >> /tmp/log
