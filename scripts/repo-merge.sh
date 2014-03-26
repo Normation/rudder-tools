d4846a9eb42752b2c455daa7bb7ca1db88aac278
Vincent Membré <vincent.membre@normation.com>
2014-03-26 10:55:01 +0100

    Repo-merge now handle branch 2.10

--- scripts/repo-merge.sh
+++ scripts/repo-merge.sh
@@ -90,13 +90,25 @@ do
 	git push
 	if [ $? -ne 0 ]; then report_error ${i} "Push"; continue; fi
 
-	# Then merge branch 2.9 with branch master
+
+        # Then merge branch 2.9 with branch 2.10
+        cd ${TEMPDIR}/${i}
+        git checkout branches/rudder/2.9 >/dev/null 2>&1 && git pull >/dev/null 2>&1 && git checkout branches/rudder/2.10 >/dev/null 2>&1 && git pull >/dev/null 2>&1
+        if [ $? -ne 0 ]; then report_error ${i} "Checkout/Pull"; continue; fi
+
+        git merge --ff-only --log branches/rudder/2.9 >/dev/null 2>&1
+        if [ $? -ne 0 ]; then report_error ${i} "Merge 2.9 -> branches/rudder/2.10"; continue; fi
+
+        git push
+        if [ $? -ne 0 ]; then report_error ${i} "Push"; continue; fi
+
+	# Then merge branch 2.10 with branch master
 	cd ${TEMPDIR}/${i}
-	git checkout branches/rudder/2.9 >/dev/null 2>&1 && git pull >/dev/null 2>&1 && git checkout master >/dev/null 2>&1 && git pull >/dev/null 2>&1
+	git checkout branches/rudder/2.10 >/dev/null 2>&1 && git pull >/dev/null 2>&1 && git checkout master >/dev/null 2>&1 && git pull >/dev/null 2>&1
 	if [ $? -ne 0 ]; then report_error ${i} "Checkout/Pull"; continue; fi
 
-	git merge --ff-only --log branches/rudder/2.9 >/dev/null 2>&1
-	if [ $? -ne 0 ]; then report_error ${i} "Merge 2.9 -> master"; continue; fi
+	git merge --ff-only --log branches/rudder/2.10 >/dev/null 2>&1
+	if [ $? -ne 0 ]; then report_error ${i} "Merge 2.10 -> master"; continue; fi
 
 	git push
 	if [ $? -ne 0 ]; then report_error ${i} "Push"; continue; fi
