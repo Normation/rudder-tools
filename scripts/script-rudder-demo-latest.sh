ded9a16bf692ab03c876bfca764e54f7a8168054
Matthieu CERDA <matthieu.cerda@normation.com>
2014-07-23 15:07:49 +0200

    Fixes #5300: Create a new version of script-rudder-demo-latest.sh that is compatible with Rudder 2.11 and Debian wheezy

--- scripts/script-rudder-demo-latest.sh
+++ scripts/script-rudder-demo-latest.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
 #####################################################################################
-# Copyright 2013 Normation SAS
+# Copyright 2013-2014 Normation SAS
 #####################################################################################
 #
 # This program is free software: you can redistribute it and/or modify
@@ -17,53 +18,84 @@
 #
 #####################################################################################
 
-# This script is the script used for our demo. It consists into several commands
-# to install the last release of Rudder 'latest' automatically.
+#
+# This script starts a demo on the current machine running Rudder's latest version.
+#
 
 set -ex
 
-#VARS
-SERVER_INSTANCE_HOST="demo-latest.rudder-project.org"
-DEMOSAMPLE="no"
-LDAPRESET="yes"
-INITPRORESET="yes"
+# Avoid any environment pollution that may cause package installation to fail
+export LANG=C LC_ALL=C
+
+# Configuration
+
+## INSTALLATION PARAMETERS
+GPG_KEYSERVER='keyserver.ubuntu.com'
+RUDDER_GPG_KEY='474A19E8'
+
+## VERSIONS
+DEBIAN_VERSION='wheezy'
+POSTGRESQL_VERSION='9.1'
+
+## RUDDER-INIT SETTINGS
+SERVER_INSTANCE_HOST='demo-latest.rudder-project.org'
+DEMOSAMPLE='no'
+LDAPRESET='yes'
+INITPRORESET='yes'
 ALLOWEDNETWORK[0]='192.168.0.0/24'
 
-##Accept Java Licence
-echo sun-java6-jre shared/accepted-sun-dlj-v1-1 select true | /usr/bin/debconf-set-selections
-#Add Rudder repo key
-apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 474A19E8
-#APT configuration
-echo "deb http://ftp.fr.debian.org/debian/ squeeze main non-free" > /etc/apt/sources.list
-echo "deb-src http://ftp.fr.debian.org/debian/ squeeze main non-free" >> /etc/apt/sources.list
-echo "deb http://security.debian.org/ squeeze/updates main" >> /etc/apt/sources.list
-echo "deb-src http://security.debian.org/ squeeze/updates main" >> /etc/apt/sources.list
-echo "deb http://ftp.fr.debian.org/debian/ squeeze-updates main" >> /etc/apt/sources.list
-echo "deb-src http://ftp.fr.debian.org/debian/ squeeze-updates main" >> /etc/apt/sources.list
-echo "deb http://www.rudder-project.org/apt-latest/ squeeze main non-free" >> /etc/apt/sources.list
-echo "deb-src http://www.rudder-project.org/apt-latest/ squeeze main non-free" >> /etc/apt/sources.list
+# Installation
 
+## 1 - Base apt configuration
+echo "deb http://ftp.fr.debian.org/debian/ ${DEBIAN_VERSION} main" > /etc/apt/sources.list
+
+## 2 - Import Debian GPG keyrings
 aptitude update
-#Packages minimum
-aptitude install -y debian-archive-keyring
-aptitude install -y rudder-inventory-ldap
+aptitude install -y debian-keyring debian-archive-keyring
+
+## 3 - Add other apt repos
+echo "deb http://security.debian.org/ ${DEBIAN_VERSION}/updates main" >> /etc/apt/sources.list
+echo "deb http://ftp.fr.debian.org/debian/ ${DEBIAN_VERSION}-updates main" >> /etc/apt/sources.list
+
+## 4 - Import Rudder GPG key
+apt-key adv --recv-keys --keyserver ${GPG_KEYSERVER} ${RUDDER_GPG_KEY}
+
+## 5 - Add Rudder repository
+echo "deb http://www.rudder-project.org/apt-latest/ ${DEBIAN_VERSION} main" > /etc/apt/sources.list.d/rudder.list
+
+## 6 - Install Rudder server
+aptitude update
 aptitude install -y rudder-server-root
 
-# Allow all connections to LDAP and PostgreSQL
+## 7 - Allow all connections to LDAP and PostgreSQL
+sed -i "s/^IP=.*$/IP=*/" /etc/default/rudder-slapd
 
-if [ -e /etc/default/rudder-slapd ]; then
-    sed -i "s/^IP=.*$/IP=*/" /etc/default/rudder-slapd
-else
-    sed -i "s/^IP=.*$/IP=*/" /etc/default/slapd
-fi
+echo "listen_addresses = '*'" >> /etc/postgresql/${POSTGRESQL_VERSION}/main/postgresql.conf
+echo "host    all         all         192.168.90.0/24       trust" >> /etc/postgresql/${POSTGRESQL_VERSION}/main/pg_hba.conf
+echo "host    all         all         10.94.94.0/24       trust" >> /etc/postgresql/${POSTGRESQL_VERSION}/main/pg_hba.conf
 
-echo "listen_addresses = '*'" >> /etc/postgresql/8.4/main/postgresql.conf
-echo "host    all         all         192.168.90.0/24       trust" >> /etc/postgresql/8.4/main/pg_hba.conf
-echo "host    all         all         10.94.94.0/24       trust" >> /etc/postgresql/8.4/main/pg_hba.conf
-/etc/init.d/postgresql restart
+service postgresql restart
 
-#Install fake nodes
-cp /root/inventories/* /var/rudder/inventories/incoming/
+## 8 - Initialize Rudder
+/opt/rudder/bin/rudder-init ${SERVER_INSTANCE_HOST} ${DEMOSAMPLE} ${LDAPRESET} ${INITPRORESET} ${ALLOWEDNETWORK[0]}
 
-# Initialize Rudder
-/opt/rudder/bin/rudder-init.sh $SERVER_INSTANCE_HOST $DEMOSAMPLE $LDAPRESET $INITPRORESET ${ALLOWEDNETWORK[0]}
+### Check that Rudder it is running OK, giving it some time to settle
+TIMEOUT=0
+while [ "${STATUS}" != "OK" ]
+do
+  ### Give Rudder 5s * 24 (120s/2m) to return OK before giving up
+  if [ ${TIMEOUT} -eq 24 ]; then echo "[WARNING] Rudder failed to respond correctly after 2m and might be broken."; break; fi
+  TIMEOUT=$((TIMEOUT + 1))
+  STATUS=$(curl -s -k https://localhost/rudder/api/status)
+  sleep 5
+done
+
+## 9 - Insert demo machines inventories (to showcase some OSes)
+git clone --depth=1 https://github.com/Normation/ldap-inventory.git
+cp ldap-inventory/inventory-fusion/src/test/resources/fusion-report/* /var/rudder/inventories/incoming/
+
+### Trigger a cf-agent run to insert the inventories
+/var/rudder/cfengine-community/bin/cf-agent -K >/dev/null 2>&1
+
+## 10 - The end
+echo "Demo installation complete."
