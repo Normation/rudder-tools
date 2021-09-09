4378362b751397c217ec164f571c85f50a99f920
Nicolas Charles <nicolas.charles@normation.com>
2021-09-09 10:41:58 +0200

    Fixes #19940: add support for ubuntu in parent ticket

--- contrib/inventory-hooks/security-upgrades.sh
+++ contrib/inventory-hooks/security-upgrades.sh
@@ -10,8 +10,13 @@ result () {
   exit 0
 }
 
-if [ ! -f /etc/debian_version ]; then
-  result -2
+# Source os-release
+. /etc/os-release
+
+if [ "debian" != "$ID" ]; then
+  if [ "ubuntu" != "$ID" ]; then
+    result -2
+  fi
 fi
 
 # ensure latest package list + check status
@@ -19,5 +24,16 @@ if ! apt-get update >/dev/null 2>&1; then
   result -1
 fi
 
-upgrades=$(apt-get upgrade --simulate | grep "^Inst " | grep ", Debian-Security:" | wc -l)
-result ${upgrades}
+# Ubuntu and Debian differ on the method
+if [ "ubuntu" = "$ID" ]; then
+  upgrades=$(apt-get upgrade --simulate | grep -i "$UBUNTU_CODENAME-security" | wc -l)
+  result ${upgrades}
+fi
+
+if [ "debian" = "$ID" ]; then
+  upgrades=$(apt-get upgrade --simulate | grep "^Inst " | grep ", Debian-Security:" | wc -l)
+  result ${upgrades}
+fi
+
+#Neither Debian nor Ubuntu
+result -2
